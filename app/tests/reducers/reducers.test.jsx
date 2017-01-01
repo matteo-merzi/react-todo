bbcf110f750c1d0ee6150dc861b0cfcc2c009ad0
Matteo Merzi <matteo.merzi@gmail.com>
2017-01-01 18:34:08 +0100

    Add todos reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -1,5 +1,6 @@
 var expect = require('expect');
 var df = require('deep-freeze-strict');
+var moment = require('moment');
 
 var reducers = require('reducers');
 
@@ -24,6 +25,40 @@ describe('Reducers', () => {
             var res = reducers.showCompletedReducer(df(false), df(action));
 
             expect(res).toEqual(true);
+        });
+    });
+
+    describe('todosReducer', () => {
+        it('should add new todo', () => {
+            var action = {
+                type: 'ADD_TODO',
+                text: 'Something to do'
+            };
+            var res = reducers.todosReducer(df([]), df(action));
+
+            expect(res.length).toEqual(1);
+            expect(res[0].text).toEqual(action.text);
+        });
+
+        // define todos array with realistic todo item
+        // generate action with matching id
+        // call reducer and assert completed flip
+        it('should toggle todo and flip completed value', () => {
+            var todos = [{
+                id: 1,
+                text: 'Something',
+                completed: false,
+                createdAt: moment().unix(),
+                completedAt: undefined
+            }];
+            var action = {
+                type: 'TOGGLE_TODO',
+                id: 1
+            };
+            var res = reducers.todosReducer(df(todos), df(action));
+
+            expect(res[0].completed).toEqual(!todos[0].completed);
+            expect(res[0].completedAt).toBeA('number');
         });
     });
 });
