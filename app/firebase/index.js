2514b4deff420b7afc1615e1b44891df3ebb01bd
Matteo Merzi <matteo.merzi@gmail.com>
2017-01-06 15:17:23 +0100

    Move firebase credentials to env variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,11 +2,11 @@ import firebase from 'firebase';
 
 try {
     var config = {
-        apiKey: "AIzaSyAf72AjXZKs7RBIy4WFJ-31p5f01rxjO4c",
-        authDomain: "mead-todo-app-738f7.firebaseapp.com",
-        databaseURL: "https://mead-todo-app-738f7.firebaseio.com",
-        storageBucket: "mead-todo-app-738f7.appspot.com",
-        messagingSenderId: "328690882309"
+        apiKey: process.env.API_KEY,
+        authDomain: process.env.AUTH_DOMAIN,
+        databaseURL: process.env.DATABASE_URL,
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGING_SENDER_ID
     };
     firebase.initializeApp(config);
 } catch (e) {
